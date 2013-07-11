3a2c2244689b7d2f4c8602fb2a01c53fabc94a95
Neo Din <neodin@tutorabc.com>
2013-07-11 16:29:43 +0800

    Initial Commit

---
+++ ShareSchemeTests/ShareSchemeTests.h
@@ -0,0 +1,13 @@
+//
+//  ShareSchemeTests.h
+//  ShareSchemeTests
+//
+//  Created by Neo Din on 13/7/11.
+//  Copyright (c) 2013年 Neo Din. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ShareSchemeTests : SenTestCase
+
+@end
---
+++ ShareSchemeTests/ShareSchemeTests.m
@@ -0,0 +1,32 @@
+//
+//  ShareSchemeTests.m
+//  ShareSchemeTests
+//
+//  Created by Neo Din on 13/7/11.
+//  Copyright (c) 2013年 Neo Din. All rights reserved.
+//
+
+#import "ShareSchemeTests.h"
+
+@implementation ShareSchemeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ShareSchemeTests");
+}
+
+@end
